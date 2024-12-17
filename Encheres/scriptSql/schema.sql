e3ba3f55d4e3c168bc40118ae71c5acfc7808f8d
AlexRovere <75780598+AlexRovere@users.noreply.github.com>
2024-12-17 14:11:34 +0100

    update sql

--- Encheres/scriptSql/schema.sql
+++ Encheres/scriptSql/schema.sql
@@ -1,58 +1,64 @@
-DROP TABLE CATEGORIES;
-DROP TABLE ENCHERES;
-DROP TABLE RETRAITS;
-DROP TABLE UTILISATEURS;
-DROP TABLE ARTICLES_VENDUS;
+drop table if exists articles_vendus;
+drop table if exists categories;
+drop table if exists retraits;
+drop table if exists encheres;
+drop table if exists utilisateurs;
 
-CREATE TABLE CATEGORIES (
-                            no_categorie   INTEGER primary key generated always as identity NOT NULL,
-                            libelle        VARCHAR(30) NOT NULL
+CREATE TABLE categories
+(
+    no_categorie INTEGER primary key generated always as identity NOT NULL,
+    libelle      VARCHAR(30)                                      NOT NULL
 );
 
-CREATE TABLE ENCHERES (
-                          no_utilisateur   INTEGER NOT NULL,
-                          no_article       INTEGER NOT NULL,
-                          date_enchere     timestamp  NOT NULL,
-                          montant_enchere  INTEGER NOT NULL,
-                          PRIMARY KEY (no_utilisateur, no_article)
+CREATE TABLE encheres
+(
+    no_utilisateur  INTEGER   NOT NULL,
+    no_article      INTEGER   NOT NULL,
+    date_enchere    timestamp NOT NULL,
+    montant_enchere INTEGER   NOT NULL,
+    PRIMARY KEY (no_utilisateur, no_article)
 );
 
-CREATE TABLE RETRAITS (
-                          no_article         INTEGER primary key generated always as identity,
-                          rue              VARCHAR(30) NOT NULL,
-                          code_postal      VARCHAR(15) NOT NULL,
-                          ville            VARCHAR(30) NOT NULL,
-                          effectue		Boolean DEFAULT FALSE
-);
-
-
-CREATE TABLE UTILISATEURS (
-                              no_utilisateur   INTEGER  primary key generated always as identity,
-                              pseudo           VARCHAR(30) NOT NULL,
-                              nom              VARCHAR(30) NOT NULL,
-                              prenom           VARCHAR(30) NOT NULL,
-                              email            VARCHAR(20) NOT NULL,
-                              telephone        VARCHAR(15),
-                              rue              VARCHAR(30) NOT NULL,
-                              code_postal      VARCHAR(10) NOT NULL,
-                              ville            VARCHAR(30) NOT NULL,
-                              mot_de_passe     VARCHAR(30) NOT NULL,
-                              credit           INTEGER NOT NULL,
-                              administrateur   bit NOT NULL
+CREATE TABLE retraits
+(
+    no_article  INTEGER primary key generated always as identity,
+    rue         VARCHAR(30) NOT NULL,
+    code_postal VARCHAR(15) NOT NULL,
+    ville       VARCHAR(30) NOT NULL
 );
 
 
-
-CREATE TABLE ARTICLES_VENDUS (
-                                 no_article                    INTEGER primary key generated always as identity,
-                                 nom_article                   VARCHAR(30) NOT NULL,
-                                 description                   VARCHAR(300) NOT NULL,
-                                 date_debut_encheres           DATE NOT NULL,
-                                 date_fin_encheres             DATE NOT NULL,
-                                 prix_initial                  INTEGER,
-                                 prix_vente                    INTEGER,
-                                 no_utilisateur                INTEGER NOT NULL,
-                                 no_categorie                  INTEGER NOT NULL
+CREATE TABLE utilisateurs
+(
+    no_utilisateur INTEGER primary key generated always as identity,
+    pseudo         VARCHAR(50) NOT NULL,
+    nom            VARCHAR(50) NOT NULL,
+    prenom         VARCHAR(50) NOT NULL,
+    email          VARCHAR(50) NOT NULL,
+    telephone      VARCHAR(20),
+    rue            VARCHAR(50) NOT NULL,
+    code_postal    VARCHAR(20) NOT NULL,
+    ville          VARCHAR(50) NOT NULL,
+    mot_de_passe   VARCHAR(100) NOT NULL,
+    credit         INTEGER     NOT NULL,
+    administrateur boolean     NOT NULL default false
 );
 
-ALTER TABLE utilisateurs add constraint UQ_utilisateurs_email UNIQUE (email)
+
+
+CREATE TABLE articles_vendus
+(
+    no_article          INTEGER primary key generated always as identity,
+    nom_article         VARCHAR(50)  NOT NULL,
+    description         VARCHAR(300) NOT NULL,
+    date_debut_encheres DATE         NOT NULL,
+    date_fin_encheres   DATE         NOT NULL,
+    prix_initial        INTEGER,
+    prix_vente          INTEGER,
+    no_utilisateur      INTEGER      NOT NULL,
+    no_categorie        INTEGER      NOT NULL,
+    retrait_effectue    boolean default false
+);
+
+ALTER TABLE utilisateurs
+    add constraint UQ_utilisateurs_email UNIQUE (email)
