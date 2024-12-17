fc375ca653fd788ef26706006596065ddfed424d
Justine44444 <137166047+Justine44444@users.noreply.github.com>
2024-12-17 14:46:15 +0100

    Update UtilisateurRepositoryImpl.java

--- Encheres/src/main/java/fr/eni/encheres/dal/UtilisateurRepositoryImpl.java
+++ Encheres/src/main/java/fr/eni/encheres/dal/UtilisateurRepositoryImpl.java
@@ -1,7 +1,11 @@
 package fr.eni.encheres.dal;
 
+import com.fasterxml.jackson.databind.util.Named;
 import fr.eni.encheres.bo.Utilisateur;
 import fr.eni.encheres.dal.interf.UtilisateurRepository;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,14 +13,28 @@ import java.util.Optional;
 
 @Repository
 public class UtilisateurRepositoryImpl implements UtilisateurRepository {
-    @Override
-    public void add(Utilisateur entity) {
 
+    private JdbcTemplate jdbcTemplate;
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    public UtilisateurRepositoryImpl(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    }
+
+    // Read
+    @Override
+    public List<Utilisateur> getAll() {
+        String sql = "SELECT no_utilisateur AS \"noUtilisateur\", " +
+                     "pseudo, nom, prenom, email, telephone, rue, " +
+                     "code_postal AS \"codePostal\", ville, mot_de_passe AS \"motDePasse\", credit " +
+                     "FROM utilisateurs";
+        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Utilisateur.class));
     }
 
     @Override
-    public List<Utilisateur> getAll() {
-        return List.of();
+    public void add(Utilisateur entity) {
+
     }
 
     @Override
